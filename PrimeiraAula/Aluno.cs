fa469fcc28e763462fbeecb1f50102b15ff59d31
Marcos Rocha <zooinm@hotmail.com>
2026-09-03 20:48:44 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiraAula/Aluno.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeiraAula
+{
+    public class Aluno: Pessoa
+    {
+    }
+}
---
+++ PrimeiraAula/Pessoa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace PrimeiraAula
+{
+    public abstract class Pessoa
+    {
+        private string nome;
+        private string matricula;
+        private DateTime datanascimento;
+
+        public string None
+        {
+            get => nome;
+            set => nome = value;
+        }
+        public string DataNascimento
+        {
+            get => datanascimento.ToString("dd/MM/yyyy");
+            set => datanascimento = DateTime.Parse(value);
+        }
+
+        public virtual string GerarMatricula()
+        {
+            var matricula = Convert.ToInt64(DateTime.Now.ToString("yyyyMMdd")) * 10;
+            return matricula.ToString();
+        }
+    }
+}
